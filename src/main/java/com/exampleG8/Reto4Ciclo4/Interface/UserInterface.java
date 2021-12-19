ae9024052ec74f1b56e22102c123389867a4a68c
sofíaN <91507303+SofiaNeira@users.noreply.github.com>
2021-12-19 09:34:26 -0500

    first commit

---
+++ src/main/java/com/exampleG8/Reto4Ciclo4/Interface/UserInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.exampleG8.Reto4Ciclo4.Interface;
+
+import com.exampleG8.Reto4Ciclo4.Model.User;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author raque
+ */
+public interface UserInterface extends MongoRepository<User, Integer> {
+     Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+}
---
+++ src/main/java/com/exampleG8/Reto4Ciclo4/Reto4Ciclo4Application.java
@@ -0,0 +1,13 @@
+package com.exampleG8.Reto4Ciclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto4Ciclo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto4Ciclo4Application.class, args);
+	}
+
+}
